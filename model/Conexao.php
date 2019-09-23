d79a2f004bb6b015013383225af8c23dfd501eef
Mario23junior <48899365+Mario23junior@users.noreply.github.com>
2019-09-23 01:32:53 -0300

    Update Conexao.php

--- model/Conexao.php
+++ model/Conexao.php
@@ -4,7 +4,7 @@
      public static $instance;
        public static function get_instance(){
            if(!isset(self::$instance)){
-              self::$instance = new PDO("mysql:host=localhost; dbname=dataBase;", "root", "sarutobi" ,
+              self::$instance = new PDO("mysql:host=localhost; dbname=dataBase;", "senhadb**", "user****" ,
               array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
               self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
